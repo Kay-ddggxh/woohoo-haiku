7c4c130edac8c931fe6ed6fcc15eb8230132f244
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-10-06 12:57:23 +0000

    Create temporary model to render base template

--- haikus/views.py
+++ haikus/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Home
 
-# Create your views here.
+
+class HomePage(generic.ListView):
+    """
+    Temporary model to render base template
+    """
+    queryset = Home
+    template_name = 'base.html'
