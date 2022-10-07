224973a2744d2aff66cd70db0b19fd78058a2726
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-10-07 15:01:39 +0000

    Create first draft of Haiku and Tanka model

--- haikus/models.py
+++ haikus/models.py
@@ -1,7 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
-class Home(models.Model):
+class Haiku(models.Model):
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="haiku_entries")
+    content = models.TextField()
+    create_date = models.DateField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User, related_name='haiku_likes', blank=True)
+    # tag = models.TextChoices()
+    # bg_image = CloudinaryField('image', default='placeholder')
+    # possibly user uploaded image
+
+    class Meta:
+        ordering = ['-create_date']
 
     def __str__(self):
-        return self
+        return self.title
+
+
+class Tanka(models.Model):
+
+    post = models.ForeignKey(
+        Haiku, on_delete=models.CASCADE, related_name='tankas')
+    name = models.CharField(max_length=60)
+    body = models.TextField()
+    create_date = models.DateField(auto_now_add=True)
+    approved = models.BooleanField(default=False)   # user approved
+
+    class Meta:
+        ordering = ['-create_date']
+
+    def __str__(self):
+        return f"{self.name} made this into a tanka: {self.body}"
--- haikus/views.py
+++ haikus/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Home
-
-
-class HomePage(generic.ListView):
-    """
-    Temporary model to render base template
-    """
-    queryset = Home
-    template_name = 'base.html'
