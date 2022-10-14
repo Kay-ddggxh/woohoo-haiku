5977a1037f52f673a0ef1e3888eef290266e4dde
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-10-14 13:25:46 +0000

    Create initial view for haiku_detail

--- haikus/urls.py
+++ haikus/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.HaikuList.as_view(), name='home')
+    path('', views.HaikuList.as_view(), name='home'),
+    path('<slug:slug>/', views.HaikuDetail.as_view(), name='haiku_detail'),
 ]
--- haikus/views.py
+++ haikus/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Haiku, Tanka
 
 
@@ -8,3 +8,25 @@ class HaikuList(generic.ListView):
     queryset = Haiku.objects.order_by('-create_date')
     template_name = 'index.html'
     # paginate_by = 10
+
+
+class HaikuDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Haiku.objects
+        haiku = get_object_or_404(queryset, slug=slug)
+        tankas = haiku.tankas.order_by('create_date')  # filter by approval
+        liked = False
+        if haiku.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "haiku_detail.html",
+            {
+                "haiku": haiku,
+                "tankas": tankas,
+                "tanka_added": False,
+                "liked": liked,
+                # add form for tanka later
+            },
+        )
