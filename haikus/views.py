2409ab7cb04b64d60ba8aacecd6b250ccc847539
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-10-30 16:14:32 +0000

    Add submit function to tanka form and render tanka

--- haikus/views.py
+++ haikus/views.py
@@ -31,3 +31,34 @@ class HaikuDetail(View):
                 "tanka_form": TankaForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Haiku.objects
+        haiku = get_object_or_404(queryset, slug=slug)
+        tankas = haiku.tankas.order_by('create_date')  # filter by approval
+        liked = False
+        if haiku.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        tanka_form = TankaForm(data=request.POST)
+
+        if tanka_form.is_valid():
+            tanka_form.instance.email = request.user.email
+            tanka_form.instance.name = request.user.username
+            tanka = tanka_form.save(commit=False)
+            tanka.post = haiku
+            tanka.save()
+        else:
+            tanka_form = TankaForm()
+
+        return render(
+            request,
+            "haiku_detail.html",
+            {
+                "haiku": haiku,
+                "tankas": tankas,
+                "tanka_added": True,
+                "liked": liked,
+                "tanka_form": TankaForm()
+            },
+        )
